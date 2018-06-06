dd469272d1e2986d71eac4eddc590fe5dbfa2f8b
root <root@localhost.localdomain>
2018-06-05 21:09:47 -0400

    Updated vigenere script to use more statistical methods. Update gitignore for vim files.

--- Week1/vigenere.py
+++ Week1/vigenere.py
@@ -8,17 +8,12 @@ __maintainer__ = "glender"
 __email__ = "None"
 __status__ = "Production"
 
-import collections, re
+import collections, string, re, math, textwrap
+from collections import defaultdict
 
 alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 message= "KCCPKBGUFDPHQTYAVINRRTMVGRKDNBVFDETDGILTXRGUD DKOTFMBPVGEGLTGCKQRACQCWDNAWCRXIZAKFTLEWRPTYC QKYVXCHKFTPONCQQRHJVAJUWETMCMSPKQDYHJVDAHCTRL SVSKCGCZQQDZXGSFRLSWCWSJTBHAFSIASPRJAHKJRJUMV GKMITZHFPDISPZLVLGWTFPLKKEBDPGCEBSHCTJRWXBAFS PEZQNRWXCVYCGAONWDDKACKAWBBIKFTIOVKCGGHJVLNHI FFSQESVYCLACNVRWBBIREPBBVFEXOSCDYGZWPFDTKFQIYCWHJVLNHIQIBTKHJVNPIST"
-letters = collections.Counter(message)
-
-DICT_WORDS = open("/usr/share/wordlists/wfuzz/general/megabeast.txt").read().splitlines() 
-
-word_file = "/usr/share/wordlists/rockyou.txt"
-WORDS = open(word_file).read().splitlines()
 
 total = len(message)
 LETTER = 'A'
@@ -75,14 +70,81 @@ def freq(message):
 		if letter != ' ':
                 	freqList[letter] = freqList[letter] + 1
 
-# Get our original frequency and Chi Statistic
-freq(message)
-CHI = chi()
+# This function is from the following:
+# https://stackoverflow.com/questions/18715688/find-common-substring-between-two-strings
+def longestSubstringFinder(string1, string2):
+    answer = ""
+    len1, len2 = len(string1), len(string2)
+    for i in range(len1):
+        match = ""
+        for j in range(len2):
+            if (i + j < len1 and string1[i + j] == string2[j]):
+                match += string2[j]
+            else:
+                if (len(match) > len(answer)): answer = match
+                match = ""
+    return answer
 
-for word in WORDS:
-	# skip words that have digits in them
-	if word.isdigit() or bool(re.search(r'\d', word)):
-		continue
+# This function is from the following: 
+# https://stackoverflow.com/questions/171765/what-is-the-best-way-to-get-all-the-divisors-of-a-number
+def divisorGenerator(n):
+    large_divisors = []
+    for i in xrange(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i*i != n:
+                large_divisors.append(n / i)
+    for divisor in reversed(large_divisors):
+        yield divisor 
+
+def findKey(split, m):
+
+    # Analyze sections separately.
+    for sec in xrange(len(split)):
+        section = split[sec]
+        section_len = len(section)
+
+        # Count letters.
+        c = dict((key, 0) for key in alphaList)
+        for char in section:
+            c[char] += 1
+
+        # Compute letter dispersion.
+        dispersion = dict((key, float(c[key]) / section_len) for key in c)
+
+        # Iterate over the letters in the alphabet
+        gs = []
+        for g in xrange(26):
+            res = 0
+            letter_g = alpha[g]
+            for i in xrange(26):
+		# get our probability of the letter happening in English
+                p_i = alphaList[alpha[i]] / 100.0
+		# get our probability
+                h_ig = dispersion[chr(((i + g) % 26) + 65)]
+                res += p_i * h_ig
+            gs.append((letter_g, res))
+
+        # Fetch best suiting value.
+        desirable = .065
+        nearest_value = 999
+        nearest_index = 0
+        for i, g in enumerate(gs):
+            difference = abs(desirable - g[1])
+            if difference < nearest_value:
+                nearest_value = difference
+                nearest_index = i
+        yield gs[nearest_index][0]
+
+def clearList():
+	# clear the frequency list and results list
+	for letter in alpha:
+		freqList[letter] = 0
+	
+	for letter in alpha:
+		resultsList[letter] = 0.0
+
+def decrypt(message, word):
 	result = ""
 	count = 0
 	maxAllowed = len(word)
@@ -114,23 +176,54 @@ for word in WORDS:
 		new_pos = (m_pos - w_pos) % 26
 		result += alpha[new_pos]
 
-	# calculate the frequency and Chi Statistic
-	freq(result)
-        if chi() < CHI:
-                CHI = chi()
-                KEY = key = word
-                DECRYPT = result
+	return result
 
-	# clear the frequency and results lists
-        for letter in alpha:
-                freqList[letter] = 0
+# Find the longest substring
+result = longestSubstringFinder(message, message)
 
-        for letter in alpha:
-                resultsList[letter] = 0.0
+# Get possible key lengths
+answer = [m.start() for m in re.finditer(result, message)]
+x = int(answer[0])
+y = int(answer[1])
+distance = int(math.sqrt( ( (x-1) - (y-1) )**2 ))
+possibleKeyLengths = list(divisorGenerator(distance))
 
-	# did we find a Chi Statistic that is relatively low? 
-	if CHI < 50.0:
-		print CHI
-		print KEY.upper()
-		print DECRYPT
-		break
+# remove the possible key lengths that are too small or too big 
+possibleKeyLengths.remove(1)
+possibleKeyLengths.remove(distance)
+
+print "Possible key lengths are:"
+print possibleKeyLengths
+
+# Split message into parts
+split = defaultdict(list)
+for i, char in enumerate(message.upper().replace(" ", "")):
+	split[i % possibleKeyLengths[0]].append(char)
+
+# attempt to find out key
+KEY = ''.join(findKey(split, possibleKeyLengths[0]))
+
+dec = decrypt(message, KEY)
+clearList()
+# count frequency
+freq(dec)
+
+# is the Chi Square value too high? 
+if chi() > 50.0:
+	# Split message into parts
+	split = defaultdict(list)
+	for i, char in enumerate(message.upper().replace(" ", "")):
+	        split[i % (possibleKeyLengths[0]+1)].append(char)
+	# is the key length one more?
+	KEY = ''.join(findKey(split, possibleKeyLengths[0]+1)) 
+
+clearList()
+dec = decrypt(message, KEY)
+
+# count frequency
+freq(dec)
+
+# is the Chi Square value too high? 
+if chi() < 50.0:
+	print KEY 
+	print dec
